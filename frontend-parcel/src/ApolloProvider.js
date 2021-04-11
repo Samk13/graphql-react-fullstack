d68b8201a1b8214290851d6997f196575d4e56aa
Samk13 <samarbid13@gmail.com>
2021-04-11 22:08:20 +0200

    :sparkles: connect apollo with react

--- frontend-parcel/src/ApolloProvider.js
+++ frontend-parcel/src/ApolloProvider.js
@@ -1,19 +1,11 @@
-// import React from 'react'
-// import App from './App'
 import { createHttpLink } from 'apollo-link-http'
-// import { ApolloProvider } from '@apollo/client/react';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000',
+  uri: 'http://localhost:5000/',
 })
 export const client = new ApolloClient({
-  uri: httpLink,
+  link: httpLink,
   cache: new InMemoryCache()
 });
 
-// export default (
-//   <ApolloProvider client={client}>
-//     <App />
-//   </ApolloProvider>
-// )
